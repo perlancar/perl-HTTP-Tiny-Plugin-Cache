c4f5443a468c46f18733dd1c3cc02d4b354d6895
perlancar (on pc-office) <perlancar@gmail.com>
2019-04-12 13:02:34 +0700

    Release v0.001

--- lib/HTTP/Tiny/Plugin/Cache.pm
+++ lib/HTTP/Tiny/Plugin/Cache.pm
@@ -12,25 +12,31 @@ use Digest::SHA;
 use File::Util::Tempdir;
 use Storable qw(store_fd fd_retrieve);
 
-sub meta {
+sub _get_cache_dir_path {
+    my ($self, $url) = @_;
+
+    my $tempdir = File::Util::Tempdir::get_user_tempdir();
+
+    my $cachedir = "$tempdir/http_tiny_plugin_cache";
+    unless (-d $cachedir) {
+        mkdir $cachedir or die "Can't mkdir '$cachedir': $!";
+    }
+
+    my $cachepath = "$cachedir/".Digest::SHA::sha256_hex($url);
+
+    ($cachedir, $cachepath);
 }
 
 sub before_request {
-    my ($class, $r) = @_;
+    my ($self, $r) = @_;
 
-    my ($self, $method, $url, $options) = @{ $r->{argv} };
+    my ($ht, $method, $url, $options) = @{ $r->{argv} };
     unless ($method eq 'GET') {
         log_trace "Not a GET response, skip caching";
         return -1; # decline
     }
 
-    my $tempdir = File::Util::Tempdir::get_user_tempdir();
-    my $cachedir = "$tempdir/http_tiny_plugin_cache";
-    #log_trace "Cache dir is %s", $cachedir;
-    unless (-d $cachedir) {
-        mkdir $cachedir or die "Can't mkdir '$cachedir': $!";
-    }
-    my $cachepath = "$cachedir/".Digest::SHA::sha256_hex($url);
+    my ($cachedir, $cachepath) = $self->_get_cache_dir_path($url);
     log_trace "Cache file is %s", $cachepath;
 
     my $maxage = $r->{config}{max_age} //
@@ -41,7 +47,7 @@ sub before_request {
         # cache does not exist or too old, we execute request() as usual and
         # later save
         $r->{cache_response}++;
-        return 0;
+        return 1; # ok
     } else {
         log_trace "Retrieving response from cache ...";
         open my $fh, "<", $cachepath
@@ -53,11 +59,22 @@ sub before_request {
 }
 
 sub after_request {
-    my ($class, $r) = @_;
+    my ($self, $r) = @_;
+
+    my ($ht, $method, $url, $options) = @{ $r->{argv} };
+
+    my ($cachedir, $cachepath) = $self->_get_cache_dir_path($url);
 
   CACHE_RESPONSE:
     {
         last unless $r->{cache_response};
+        if ($r->{config}{cache_if}) {
+            if (ref $r->{config}{cache_if} eq 'Regexp') {
+                last unless $r->{response}{status} =~ $r->{config}{cache_if};
+            } else {
+                last unless $r->{config}{cache_if}->($self, $r->{response});
+            }
+        }
         log_trace "Saving response to cache ...";
         open my $fh, ">", $cachepath
             or die "Can't create cache file '$cachepath' for '$url': $!";
@@ -65,16 +82,19 @@ sub after_request {
         close $fh;
         undef $r->{cache_response};
     }
-    0;
+    1; # ok
 }
 
 1;
 # ABSTRACT: Cache HTTP::Tiny responses
 
+=for Pod::Coverage .+
+
 =head1 SYNOPSIS
 
  use HTTP::Tiny::Plugin 'Cache' => {
-     max_age => '3600', # defaults to HTTP_TINY_PLUGIN_CACHE_MAX_AGE or CACHE_MAX_AGE or 86400
+     max_age  => '3600',     # defaults to HTTP_TINY_PLUGIN_CACHE_MAX_AGE or CACHE_MAX_AGE or 86400
+     cache_if => qr/^[23]/,  # optional, default is to cache all responses
  };
 
  my $res  = HTTP::Tiny::Plugin->new->get("http://www.example.com/");
@@ -86,8 +106,9 @@ sub after_request {
 This plugin can cache responses to cache files.
 
 Currently only GET requests are cached. Cache are keyed by SHA256-hex(URL).
-Error responses are also cached. Currently no cache-related HTTP request or
-response headers (e.g. C<Cache-Control>) are respected.
+Error responses are also cached (unless you configure C</cache_if>). Currently
+no cache-related HTTP request or response headers (e.g. C<Cache-Control>) are
+respected.
 
 
 =head1 CONFIGURATION
@@ -95,6 +116,11 @@ response headers (e.g. C<Cache-Control>) are respected.
 =head2 max_age
 
 Int.
+
+=head2 cache_if
+
+Regex or code. If regex, then will be matched against response status. If code,
+will be called with arguments: C<< ($self, $response) >>.
 
 
 =head1 ENVIRONMENT
